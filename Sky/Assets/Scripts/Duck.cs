45507647c75d4135347f59b4e0336a5e2bfdfcb7
Brandon Rivera-Melo <hectorrivera-melo@Hectors-Air.attlocal.net>
2015-08-30 12:21:37 -0500

    Ducks Flying
    duck code + ducks flapping

---
+++ Sky/Assets/Scripts/Duck.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Duck : MonoBehaviour {
+
+	public float moveSpeed;
+	public float currentSpeed;
+
+	// Use this for initialization
+	void Awake () {
+		moveSpeed = 2f;
+		currentSpeed = moveSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > 6) {
+			currentSpeed = -moveSpeed;
+			transform.localScale = new Vector3 (4,4,1);
+		}
+		else if (transform.position.x < -6){
+			currentSpeed = moveSpeed;
+			transform.localScale = new Vector3 (-4,4,1);
+		}
+		rigidbody2D.velocity = Vector2.right * currentSpeed;
+	}
+}
